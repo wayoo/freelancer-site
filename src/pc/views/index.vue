1b2378be00b970133565c0a24a0d53092cf8a5c5
wangyuzju <wangyu0248@gmail.com>
2019-08-28 19:54:30 +0800

    add

--- src/pc/views/index.vue
+++ src/pc/views/index.vue
@@ -14,6 +14,21 @@
             </section>
         </div>
 
+        <div class="advantage">
+            <div class="container">
+                <div class="banner-advance"></div>
+                <div class="content">
+                    <div class="title">我们能为您的企业提供</div>
+                    <div class="text">壹薪网能够通过专业化的成本分析模型来为您的企业量身定做灵活用工方案，
+                        以合法、合规的方式为您的企业<em>减少社保成本</em>；
+                        通过对企业临时人员改造劳动关系来<em>降低人员流动成本</em>，
+                        并且可以有效解决<em>劳务用工无发票</em>的问题；
+                        我们将用平台化来安全有效地为您连接每一位服务者，
+                        使您的企业有效<em>减少季节性用工成本</em>；
+                        并且我们相比传统用工平台更加合规化，选择壹薪网可以让您的企业最大化<em>降低用工风险</em>。</div>
+                </div>
+            </div>
+        </div>
 
         <div class="feature">
             <div class="container">
@@ -50,7 +65,7 @@
                                 'bl': n % 3 === 2,
                                 'bb': n < 3,
                             }">
-                                <img :src="require(`../../assets/pc/images/feature/0000/${n+1}.png`)" alt="">
+                                <img :src="require(`../../assets/pc/images/feature/0001/${n+1}.png`)" alt="">
                                 <div class="title">{{ item.title }}</div>
                                 <div class="desc" v-html="item.desc"></div>
                             </div>
@@ -105,49 +120,49 @@ export default {
                 },
                 {
                     title: '电子签名',
-                    desc: '对接公安部公民网络身份识别系统<br>对个人真实身份进行认证'
+                    desc: '采用安全可靠的电子签名技术(E签宝)<br>实现安全合规有效的服务协议'
                 },
                 {
                     title: '个体工商户',
-                    desc: '对接公安部公民网络身份识别系统<br>对个人真实身份进行认证'
+                    desc: '在线快速协助办理及验证个体工商户信息<br>确保每个服务者都是个体工商户'
                 },
                 {
                     title: '即时抢单',
-                    desc: '对接公安部公民网络身份识别系统<br>对个人真实身份进行认证'
+                    desc: '企业发布任务包后实时推送服务者<br>随时随地接单获得任务佣金'
                 },
                 {
                     title: '结算透明',
-                    desc: '对接公安部公民网络身份识别系统<br>对个人真实身份进行认证'
+                    desc: '任务完成后商家根据标准批量验收<br>验收结果透明、高效'
                 },
                 {
                     title: '安全钱包',
-                    desc: '对接公安部公民网络身份识别系统<br>对个人真实身份进行认证'
+                    desc: '任务结算完成，佣金立即转入个人账户<br>安全快速且随时可查'
                 }
             ],
             feature_two: [
                 {
-                    title: '实名认证',
-                    desc: '对接公安部公民网络身份识别系统<br>对个人真实身份进行认证'
+                    title: '一键发单',
+                    desc: '无需填写复杂的招工启示，简单填写用工需求<br>即可快速发单，还支持自动化定时任务'
                 },
                 {
-                    title: '电子签名',
-                    desc: '对接公安部公民网络身份识别系统<br>对个人真实身份进行认证'
+                    title: '智能匹配',
+                    desc: '智能快速匹配所需服务者，为保护企业隐私<br>也可以灵活指定相关服务者'
                 },
                 {
-                    title: '个体工商户',
-                    desc: '对接公安部公民网络身份识别系统<br>对个人真实身份进行认证'
+                    title: '任务验收',
+                    desc: '针对服务者完成的任务，可以根据企业标准<br>高效安全的批量审核验收'
                 },
                 {
-                    title: '即时抢单',
-                    desc: '对接公安部公民网络身份识别系统<br>对个人真实身份进行认证'
+                    title: '用工放心',
+                    desc: '根据企业的需求随时随地可以<br>查看服务者认证信息及任务记录'
                 },
                 {
-                    title: '结算透明',
-                    desc: '对接公安部公民网络身份识别系统<br>对个人真实身份进行认证'
+                    title: '覆盖广泛',
+                    desc: '业务覆盖各行各业<br>服务者可覆盖全国各地'
                 },
                 {
-                    title: '安全钱包',
-                    desc: '对接公安部公民网络身份识别系统<br>对个人真实身份进行认证'
+                    title: '平台保障',
+                    desc: '平台固化整个任务流程及佣金结算的流程记录<br>用工安全合规有保障'
                 }
             ]
         }
@@ -158,7 +173,7 @@ export default {
 <style lang="less">
 .page-index {
     .section-banner {
-        padding-top: 300px;
+        padding-top: 100px;
         position: relative;
         overflow: hidden;
         padding-bottom: 180px;
@@ -174,7 +189,7 @@ export default {
     .intro {
         position: absolute;
         left: 0;
-        top: 360px;
+        top: 160px;
 
 
         .main-title {
@@ -210,6 +225,58 @@ export default {
 
             display: block;
             text-align: center;
+        }
+    }
+
+    .advantage {
+        position: relative;
+        overflow: hidden;
+        margin-bottom: 220px;
+
+        .container {
+            position: relative;
+            z-index: 1000;
+        }
+
+        &::before {
+            top: 37px;
+            content: ' ';
+            z-index: 10;
+            position: absolute;
+            width: 100%;
+            height: 220px;
+            background: #f1f5fc;
+        }
+
+        .banner-advance {
+            width: 354px;
+            height: 295px;
+            float: left;
+            background: url("../../assets/pc/advance.png");
+        }
+        .content {
+            width: 780px;
+            float: right;
+            text-align: right;
+            padding-top: 60px;
+
+            .title {
+                font-size: 36px;
+                color: #032c64;
+                margin-bottom: 20px;
+            }
+
+            .text {
+                text-align: left;
+                font-size: 16px;
+                color: #032c64;
+                line-height: 30px;
+
+                em {
+                    color: #4a8cff;
+                    font-style: normal;
+                }
+            }
         }
     }
 
