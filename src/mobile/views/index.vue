c726a312ce0181ee4df0e5673cb43f828c02d579
wangyuzju <wangyu0248@gmail.com>
2020-04-28 11:41:11 +0800

    fix bug

--- src/mobile/views/index.vue
+++ src/mobile/views/index.vue
@@ -117,6 +117,15 @@
         </div>
 
         <div class="footer">蜗牛企服（北京）科技有限公司™   鲁ICP备19039591号-1</div>
+        <div style="width:300px;margin:0 auto; padding:20px 0;font-size: 12px; text-align: center;">
+            <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=37028502200443"
+                style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+                <img src="//s.wdyedu.com/build/img/batb-d0289dc0a4.png" style="float:left;"/>
+                <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">
+                鲁公网安备 37028502200443号
+                </p>
+            </a>
+        </div>
     </div>
 </template>
 
